d60842bc13253a9540eee9390c0b101024157323
Bignone <eduardobustosmiranda@hotmail.com>
2018-04-16 21:03:53 +0200

    pruebaGIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NvD3Component } from 'ng2-nvd3';
 
+// d3 and nvd3 should be included somewhere
+import 'd3';
+import 'nvd3';
 
 @NgModule({
   declarations: [
